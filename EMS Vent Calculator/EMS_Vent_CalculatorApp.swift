6677821cc5e1ced3ce72e47d1e893e2e06b7dcb8
NickJennings <spacemedic911@gmail.com>
2022-11-15 23:59:26 -0600

    Initial Commit

---
+++ EMS Vent Calculator/EMS_Vent_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMS_Vent_CalculatorApp.swift
+//  EMS Vent Calculator
+//
+//  Created by NICK JENNINGS on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct EMS_Vent_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
